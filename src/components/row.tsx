6add7eee423141b470df5f11c3e18e23fde596bb
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-02-11 20:42:22 +0300

    add some styles to row inputs

--- src/components/row.tsx
+++ src/components/row.tsx
@@ -1,20 +1,19 @@
-import { useReducer } from "react";
 import { useForm } from "react-hook-form";
-import { letterReducer } from "../reducers/letters-reducer";
 
 type FormValues = {
   letters: string[];
 };
 
-const initialState = { letters: ["", "", "", "", ""] };
+interface RowProps {
+  state: FormValues;
+}
 
-export const Row = () => {
-  const [state, dispatch] = useReducer(letterReducer, initialState);
+export const Row = ({ state }: RowProps) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>({ defaultValues: initialState });
+  } = useForm<FormValues>({ defaultValues: { letters: state.letters } });
 
   const onSubmit = (data: FormValues) => {
     console.log(data);
@@ -22,9 +21,19 @@ export const Row = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form className="flex space-x-4" onSubmit={handleSubmit(onSubmit)}>
         {state.letters.map((_, i) => (
-          <input key={i} className="border" {...register(`letters.${i}`)} />
+          <input
+            key={i}
+            type="text"
+            className="w-10 h-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center"
+            maxLength={1}
+            {...register(`letters.${i}`, {
+              required: true,
+              maxLength: 1,
+              pattern: /^[a-zA-Z]+$/i,
+            })}
+          />
         ))}
         <input type="submit" className="hidden" />
       </form>
--- src/components/wordle.tsx
+++ src/components/wordle.tsx
@@ -1,9 +1,21 @@
+import { useReducer } from "react";
+
 import { Row } from "./row";
+import { letterReducer } from "../reducers/letters-reducer";
+
+const initialState = { letters: ["", "", "", "", ""] };
 
 export const Wordle = () => {
+  const [state, dispatch] = useReducer(letterReducer, initialState);
+
   return (
-    <div>
-      <Row />
+    <div className="space-y-4">
+      <Row state={state} />
+      <Row state={state} />
+      <Row state={state} />
+      <Row state={state} />
+      <Row state={state} />
+      <Row state={state} />
     </div>
   );
 };
