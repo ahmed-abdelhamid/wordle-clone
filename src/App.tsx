82f7f01b56cc98a6c3440b33af58410e83047d55
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-02-11 17:40:00 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-full w-full flex flex-col items-center p-6">
+      <h1 className="text-3xl font-bold">Wordle</h1>
+    </div>
+  );
+}
+
+export default App;
